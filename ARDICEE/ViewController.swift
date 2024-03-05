008b0627db1cb070ee6eba259207724402e8ac6f
priyam kumar das <37584901+priyam9669@users.noreply.github.com>
2024-03-05 23:07:23 +0530

    Initial Commit

---
+++ ARDICEE/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  ARDICEE
+//
+//  Created by Priyam on 05/03/24.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        let cube = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        cube.materials = [material]
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.geometry = cube
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        
+//        // Set the scene to the view
+//        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    
+}
