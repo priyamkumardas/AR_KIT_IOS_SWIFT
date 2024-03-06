bbe29e5cbad813662564b330a766b293ac2d2e20
priyam kumar das <37584901+priyam9669@users.noreply.github.com>
2024-03-06 23:21:57 +0530

    demo video

--- ARDICEE/ViewController.swift
+++ ARDICEE/ViewController.swift
@@ -15,14 +15,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        let cube = SCNSphere(radius: 0.2)
+        let cube = SCNSphere(radius: 0.1)
         
         let material = SCNMaterial()
         material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
@@ -44,7 +44,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -54,6 +54,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation,types: .existingPlaneUsingExtent)
+            if let hitResults = results.first{
+                
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor{
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            planeNode.transform=SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+        }else{
+            return
+        }
     }
 
     
